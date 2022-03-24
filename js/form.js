70477664dd7680c876c43fed61946661d16dc78f
jnthns <jonathanjjshek@gmail.com>
2022-03-24 13:49:26 -0700

    completed form

--- js/form.js
+++ js/form.js
@@ -14,27 +14,43 @@ window.addEventListener('DOMContentLoaded', event => {
             if (deploymentKey == '' || flagKey == '') {
                 alert('Please enter your Experiment Keys');
             } else if (deploymentKey.startsWith('client-') && flagKey.length > 0) {
-                localStorage.setItem('deploymentKey', deploymentKey);
-                localStorage.setItem('flagKey', flagKey);
+                if (userId || deviceId) {
+                    if (userProperties) {
+                        try {
+                            var parsedJSON = JSON.parse(userProperties);
+
+                            if (parsedJSON && typeof parsedJSON === 'object') {
+                                const user = {
+                                    user_id: userId,
+                                    device_id: deviceId,
+                                    user_properties: parsedJSON
+                                }
+
+                                document.querySelector('#idCheck').innerHTML = JSON.stringify(user, undefined, 4);
+                            } else {
+                                document.querySelector('#idCheck').innerHTML = 'User properties not a valid JSON';
+                            }
+
+                        } catch (e) {
+                            console.error(e)
+                            document.querySelector('#idCheck').innerHTML = 'Not a valid JSON';
+                        }
+                    } else {
+                        const user = {
+                                user_id: userId,
+                                device_id: deviceId
+                            };
+
+                        document.querySelector('#idCheck').innerHTML = JSON.stringify(user, undefined, 4);
+                    }
+                    
+                } else if (!userId && !deviceId) {
+                    document.querySelector('#idCheck').innerHTML = 'Need to send either User ID or Device ID';
+                }
             }
 
-            const experiment = Experiment.Experiment.initializeWithAmplitudeAnalytics(deploymentKey);
+            // const experiment = Experiment.Experiment.initializeWithAmplitudeAnalytics(deploymentKey);
             document.querySelector('#loadMessage').innerHTML = 'Experiment Initialized';
-
-            if (!userId) {
-                document.querySelector('#idCheck').innerHTML = 'Make sure to send Device ID if not setting a user ID';
-                if (userId || deviceId) {
-                    const user = {
-                        user_id: userId,
-                        device_id: deviceId,
-                        user_properties: userProperties
-                    };
-
-                    document.querySelector('#idCheck').innerHTML = JSON.stringify(user, undefined, 4);
-                }
-            } else if (!userId && !deviceId) {
-                document.querySelector('#idCheck').innerHTML = 'Need to send either User ID or Device ID';
-            } 
 
             // Fetch/Variant and Display
             const response = document.querySelector('#displayResponse');
@@ -62,7 +78,6 @@ window.addEventListener('DOMContentLoaded', event => {
             })
         })
     }
-
     
 });
 
