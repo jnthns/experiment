076660446809d980505edd0d79a62d7d04cb5f9c
jnthns <jonathanjjshek@gmail.com>
2022-03-24 16:19:09 -0700

    sync

---
+++ js/analytics.js
@@ -0,0 +1,74 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.3 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
+// 
+// Scripts
+
+// Amplitude
+// const apiKey = 'f2e177f8439cb99ac2297f32dc892a1c'
+// const userId = 'Visitor 2';
+// const deviceId = '2ea0e37b-264a-5cfa-b449-424703520a68';
+
+var amplitudeInstance;
+var analyticsInitialized = false;
+
+// Events
+// var homepageViewed = () => {amplitude.logEvent("Viewed Home Page", eventProperties)};
+var menuExpanded = () => {amplitude.logEvent("Expanded Menu")};
+var menuClosed = () => {amplitude.logEvent("Closed Menu")};
+
+window.addEventListener('DOMContentLoaded', event => {
+    const analyticsInit = document.querySelector('#analyticsInit');
+    if (analyticsInit) {
+        analyticsInit.addEventListener('click', () => {
+            var analyticsKey = document.querySelector('#analyticsKey').value;
+            var userId = document.querySelector('#userId').value;
+
+            if (analyticsKey.length != 32) {
+                alert('Please enter your Analytics Key');
+
+            } else if (analyticsKey.length == 32) {
+
+                if (userId) {
+                    amplitudeInstance = amplitude.getInstance().init(analyticsKey, userId);
+                    document.querySelector('#analyticsLoadMessage').innerHTML = 'Analytics Initialized for ' + String(userId);
+                    
+                    localStorage.setItem('exp_test_analytics_key', analyticsKey);
+                    localStorage.setItem('exp_test_user_id', userId);
+
+                } else {
+                    amplitudeInstance = amplitude.getInstance().init(analyticsKey);
+                    document.querySelector('#analyticsLoadMessage').innerHTML = 'Analytics Initialized without User ID';
+                    
+                    localStorage.setItem('exp_test_analytics_key', analyticsKey);
+                }
+            }
+        })
+
+        analyticsInitialized = true;
+    }
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+            document.body.classList.toggle('sb-sidenav-toggled');
+        }
+
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+
+            if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+                menuClosed();
+            } else {
+                menuExpanded();
+            }
+        });
+    }
+});
+
+
---
+++ js/experiment.js
@@ -0,0 +1,97 @@
+// const deploymentKey = 'client-19YzfjOdwGMpwK6OU0uKGz8rqXh57QSV';
+// const flagKey = 'sidebar';
+var userObject = {};
+
+window.addEventListener('DOMContentLoaded', event => {
+    const onKeySubmit = document.querySelector('#onKeySubmit');
+    if (onKeySubmit) {
+        onKeySubmit.addEventListener('click', () => {
+            const deploymentKey = document.querySelector('#deploymentKey').value;
+            const flagKey = document.querySelector('#flagKey').value;
+            const userId = document.querySelector('#userId').value;
+            const deviceId = document.querySelector('#deviceId').value;
+            const userProperties = document.querySelector('#userProperties').value;
+            
+            if (deploymentKey == '' || flagKey == '') {
+                alert('Please enter your Experiment Keys');
+            } else if (deploymentKey.startsWith('client-') && flagKey.length > 0) {
+                if (userId || deviceId) {
+                    if (userProperties) {
+                        try {
+                            var parsedJSON = JSON.parse(userProperties);
+
+                            if (parsedJSON && typeof parsedJSON === 'object') {
+                                const user = {
+                                    user_id: userId,
+                                    device_id: deviceId,
+                                    user_properties: parsedJSON
+                                }
+
+                                userObject = user;
+
+                                document.querySelector('#idCheck').innerHTML = JSON.stringify(user, undefined, 4);
+                            } else {
+                                document.querySelector('#idCheck').innerHTML = 'User properties not a valid JSON';
+                            }
+
+                        } catch (e) {
+                            console.error(e)
+                            document.querySelector('#idCheck').innerHTML = 'Not a valid JSON';
+                        }
+                    } else {
+                        const user = {
+                                user_id: userId,
+                                device_id: deviceId
+                            };
+
+                        userObject = user;
+
+                        document.querySelector('#idCheck').innerHTML = JSON.stringify(user, undefined, 4);
+                    }
+                    
+                } else if (!userId && !deviceId) {
+                    document.querySelector('#idCheck').innerHTML = 'Need to send either User ID or Device ID';
+                }
+            }
+
+            const experiment = Experiment.Experiment.initializeWithAmplitudeAnalytics(deploymentKey);
+            document.querySelector('#loadMessage').innerHTML = 'Experiment Initialized';
+
+            // Fetch/Variant and Display
+            const response = document.querySelector('#displayResponse');
+            document.querySelectorAll('.experiment').forEach(button => {
+                button.addEventListener('click', async () => {
+                    if (button.innerHTML == 'Fetch') {
+                        const fetchedResponse = await experiment.fetch(userObject);
+
+                        if (fetchedResponse) {
+                            response.innerHTML = JSON.stringify(fetchedResponse, undefined, 4);    
+                        } else {
+                            response.innerHTML = "Error with Fetch Request";
+                        }
+
+                    } else if (button.innerHTML == 'Variant') {
+                        const variant = experiment.variant(flagKey);
+
+                        if (variant) {
+                            response.innerHTML = JSON.stringify(variant, undefined, 4);
+                        } else {
+                            response.innerHTML = "Error with Variant Request";
+                        }
+                    } else if (button.innerHTML == 'Exposure') {
+                        const exposure = experiment.exposure(flagKey);
+
+                        if (exposure) {
+                            response.innerHTML = JSON.stringify(exposure, undefined, 4);
+                        } else {
+                            response.innerHTML = "Error with Exposure";
+                        }
+                    }
+                })
+            })
+        })
+    }
+    
+});
+
+
